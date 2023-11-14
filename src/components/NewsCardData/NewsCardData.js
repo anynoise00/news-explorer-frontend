ed60fc6b18ce9ec45ccf2fec441024b869467449
anynoise00 <anynoise00@gmail.com>
2023-11-14 13:58:59 -0300

    dev: change article image alt dinamically

--- src/components/NewsCardData/NewsCardData.js
+++ src/components/NewsCardData/NewsCardData.js
@@ -14,11 +14,11 @@ function NewsCardData(props) {
       <img
         className='news-card__image'
         src={props.data.urlToImage}
-        alt='Uma imagem representativa do artigo'
+        alt={`Uma imagem representativa do artigo: ${props.data.title}`}
       />
       <div className='news-card__info-container'>
         <h4 className='news-card__date'>{props.data.publishedAt}</h4>
-        <h2 className='news-card__header'>{props.data.title}</h2>
+        <h2 className='news-card__header'></h2>
         <p className='news-card__description'>{props.data.description}</p>
         <h3 className='news-card__source'>{props.data.source.name}</h3>
       </div>
