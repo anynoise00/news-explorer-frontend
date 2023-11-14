dea5d886192654aade73be7f0ad5aab56f96dcf3
anynoise00 <anynoise00@gmail.com>
2023-11-14 15:44:13 -0300

    dev: remove ellipsis from text truncation

--- src/components/NewsCardData/NewsCardData.js
+++ src/components/NewsCardData/NewsCardData.js
@@ -18,7 +18,7 @@ function NewsCardData(props) {
       />
       <div className='news-card__info-container'>
         <h4 className='news-card__date'>{props.data.publishedAt}</h4>
-        <h2 className='news-card__header'></h2>
+        <h2 className='news-card__header'>{props.data.title}</h2>
         <p className='news-card__description'>{props.data.description}</p>
         <h3 className='news-card__source'>{props.data.source.name}</h3>
       </div>
